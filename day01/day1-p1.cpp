c647ebeade3d20e0530b59f264171af206aebda9
AugFJTan <fj_tan@hotmail.com>
2025-12-02 21:39:46 +0800

    Add Day 1 Part 1 solution

---
+++ day01/day1-p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	std::ifstream file;
+	std::string data;
+
+	int value = 50;
+	int counter = 0;
+
+	file.open("input.txt");
+	while(std::getline(file, data)) {
+		int sign = data[0] == 'L' ? -1 : 1;
+		int delta = sign * std::stoi(data.substr(1));
+
+		value += delta;
+		value = value % 100;
+
+		if (value < 0)
+			value = 100 + value;
+
+		if (value == 0)
+			counter++;
+	}
+
+	std::cout << counter << std::endl;
+
+	return 0;
+}
